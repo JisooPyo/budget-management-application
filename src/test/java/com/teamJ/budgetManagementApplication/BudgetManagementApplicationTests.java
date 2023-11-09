da26140a1b8b7f93fa93144f39b1a9d51b9877d0
JisooPyo <audwl326@gmail.com>
2023-11-09 11:40:10 +0900

    [init] first commit

---
+++ src/test/java/com/teamJ/budgetManagementApplication/BudgetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamJ.budgetManagementApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
