bc8abcc3e5eb7a98bd1f90f052bc1e558a9da162
JisooPyo <audwl326@gmail.com>
2023-11-10 22:11:57 +0900

    chore: 필요없는 주석 제거

--- src/main/java/com/teamJ/budgetManagementApplication/common/exception/CustomErrorCode.java
+++ src/main/java/com/teamJ/budgetManagementApplication/common/exception/CustomErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum CustomErrorCode {
-    // 예시 에러코드입니다.
+
     USER_ALREADY_EXIST(HttpStatus.BAD_REQUEST.value(), "이미 존재하는 사용자입니다.");
 
     private final int errorCode;
