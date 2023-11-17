591bedadbcd97fd4e6eabe11910c5f3184b4cf39
JisooPyo <audwl326@gmail.com>
2023-11-17 22:45:34 +0900

    feat: ErrorCode 추가

--- src/main/java/com/teamJ/budgetManagementApplication/common/exception/CustomErrorCode.java
+++ src/main/java/com/teamJ/budgetManagementApplication/common/exception/CustomErrorCode.java
@@ -8,7 +8,11 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum CustomErrorCode {
 
-    USER_ALREADY_EXIST(HttpStatus.BAD_REQUEST.value(), "이미 존재하는 사용자입니다.");
+    USER_ALREADY_EXISTS(HttpStatus.BAD_REQUEST.value(), "이미 존재하는 사용자입니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "사용자가 존재하지 않습니다."),
+    BUDGET_ALREADY_EXISTS(HttpStatus.BAD_REQUEST.value(), "이미 설정된 예산입니다."),
+    BUDGET_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "예산이 존재하지 않습니다."),
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "카테고리가 존재하지 않습니다.");
 
     private final int errorCode;
     private final String errorMessage;
