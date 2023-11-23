b4b391e7ad7a7177b766b1ed4af0a292763b5a8e
JisooPyo <audwl326@gmail.com>
2023-11-23 12:43:03 +0900

    feat: CustomErrorCode 예산 수정에 필요한 에러코드 추가

--- src/main/java/com/teamJ/budgetManagementApplication/common/exception/CustomErrorCode.java
+++ src/main/java/com/teamJ/budgetManagementApplication/common/exception/CustomErrorCode.java
@@ -12,7 +12,9 @@ public enum CustomErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "사용자가 존재하지 않습니다."),
     BUDGET_ALREADY_EXISTS(HttpStatus.BAD_REQUEST.value(), "이미 설정된 예산입니다."),
     BUDGET_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "예산이 존재하지 않습니다."),
-    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "카테고리가 존재하지 않습니다.");
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "카테고리가 존재하지 않습니다."),
+    ACCESS_DENIED(HttpStatus.BAD_REQUEST.value(), "잘못된 접근입니다."),
+    BUDGET_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "예산-카테고리가 존재하지 않습니다.");
 
     private final int errorCode;
     private final String errorMessage;
