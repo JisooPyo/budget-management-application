097056490aca5a80281685b0fb3c85f58ea9fb6b
JisooPyo <audwl326@gmail.com>
2023-11-23 21:08:49 +0900

    refactor: ExpenseCreateRequestDto import 제거

--- src/main/java/com/teamJ/budgetManagementApplication/expense/dto/ExpenseCreateRequestDto.java
+++ src/main/java/com/teamJ/budgetManagementApplication/expense/dto/ExpenseCreateRequestDto.java
@@ -1,8 +1,6 @@
 package com.teamJ.budgetManagementApplication.expense.dto;
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
